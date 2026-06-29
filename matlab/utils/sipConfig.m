616117ab513ac527bd3ab93c00bd021a22e53a2a
Sergio Conde <124182273+sergio-conde@users.noreply.github.com>
2026-06-29 09:18:36 +0200

    Update sipConfig.m

--- matlab/utils/sipConfig.m
+++ matlab/utils/sipConfig.m
@@ -19,20 +19,20 @@ cfg = []; % Initializing output
 
 % Folder structure as default
 % files configuration %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-cfg.folder.root           = '\\vs03\VS03-NandB-1\tara\ephys_rat_sip';       % project's root folder
-cfg.folder.raw            = [cfg.folder.root '\Data_collection\raw'];       % data's raw matlab files folder
-cfg.folder.proc           = [cfg.folder.root '\Data_collection\processed']; % data's preprocessed folder
-cfg.folder.analysis       = [cfg.folder.root '\Data_analysis'];             % data's analysis folder
-cfg.folder.publications   = [cfg.folder.root '\Publications'];              % data's Publications folder
-cfg.folder.support        = [cfg.folder.proc '\support'];                   % data's support files folder
-cfg.folder.level_name     = {'cohort','animal','session'};                  % labels of each organization level
-cfg.folder.folder_coding  = {'cohort*','w*','*'};                           % string coding for each level (folders)
+cfg.folder.root          = '\\vs03\VS03-NandB-1\tara\ephys_rat_sip';       % project's root folder
+cfg.folder.raw           = [cfg.folder.root '\Data_collection\raw'];       % data's raw matlab files folder
+cfg.folder.proc          = [cfg.folder.root '\Data_collection\processed']; % data's preprocessed folder
+cfg.folder.analysis      = [cfg.folder.root '\Data_analysis'];             % data's analysis folder
+cfg.folder.publications  = [cfg.folder.root '\Publications'];              % data's Publications folder
+cfg.folder.support       = [cfg.folder.proc '\support'];                   % data's support files folder
+cfg.folder.levelName     = {'cohort','animal','session'};                  % labels of each organization level
+cfg.folder.folderCoding  = {'cohort*','w*','*'};                           % string coding for each level (folders)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % raw data info %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-cfg.data.nrats        = 16;
-cfg.data.nlynxs_files = {'ntt','csc'};
-cfg.data.nlynxs_label = {'TT','CSC'};
+cfg.data.nRats        = 16;
+cfg.data.nlynxsFiles = {'ntt','csc'};
+cfg.data.nlynxsLabel = {'TT','CSC'};
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 for iParam = 1:length(reqConfig)
@@ -43,11 +43,55 @@ function cfg = addConfig(cfg,param)
 
 switch param
     case 'task'
+        cfg.task.sessionEpochs  = {'early','transition','late'};
+        cfg.task.cueDuration    = 5;                                % cue duration in sec
+        cfg.task.wois = {
+            'pre_all', ...
+            'pre_event', ...
+            'pre_cue', ...
+            'cue', ... 
+            'intake', ...
+            'event', ...
+            'no_event'};
+        cfg.task.manipulations  = {'none', 'baseline1', 'baseline2',...
+            'habituation', 'baseline', 'saline', 'dry', 'quinine', 'adlib',...
+            'veh', 'mtep', 'extinction'};
+
     case 'behavior'
+        cfg.behavior.drinkGroup    = {'ld','hd'};                      % High and low drinkers label
+        cfg.behavior.lickLabel     = {'plus','minus'};                 % High and low drinkers label
+        cfg.behavior.events        = {'lick','headent'};               % main behavioral events
+        cfg.behavior.interBout     = [1 1.5];                          % maximum time between behavioral events to be considered as part of the same bout
+        cfg.behavior.min_bout_dur  = [2 1];                            % minimum duration of one bout
+
     case 'video'
+        cfg.video.vrate        = 25;         % video sampling rate in frames/s
+        cfg.video.videoRes     = [576 720];  % tracking video resolution
+        cfg.video.box.width    = 32.5;
+        cfg.video.box.length   = 31.5;
+        cfg.video.box.hight    = 40;
+        cfg.video.box.external = 40;
+
+        video_res                = cfg.video.videoRes;
+        cfg.video.box.video      = [1 1 video_res(2) video_res(2) 1;1 video_res(1) video_res(1) 1 1];
+        cfg.video.box.grid       = [125 125 465 465 125;85 440 440 85 85];
+        cfg.video.box.cue        = [480 422];
+        cfg.video.box.mag        = [465,270];
+        cfg.video.box.drink      = [75 270];
+        cfg.video.box.max_dist   = 5;  % [cm] distances less than this denote proximity to, for example, the cue
     case 'spike'
     case 'lfp'
     case 'colors'
+        cfg.color.rat     = jet(16);
+        cfg.color.session = summer(25);
+        cfg.color.woi     = [60 101 176;237 123 46]/255;
+        cfg.color.event   = [14 102 62;163 60 15]/255;
+        cfg.color.area    = [50 168 82;90 50 168]/255;
+        cfg.color.ld      = [128 179 255]/255;
+        cfg.color.hd      = [0 51 128]/255;
+        cfg.color.plus    = [200 52 2]/255;
+        cfg.color.minus   = 0.65 * ones(1,3);
+        cfg.color.gray    = 0.65 * ones(1,3);
     otherwise
         warning(['WARNING: Configuration request invalid\n' ...
             'The following field was ignored: \n'])
@@ -84,72 +128,34 @@ end
 
 
 
-cfg.analysis.rat_ids        = rat_ids;                          % 
-cfg.analysis.nrats          = size(rat_ids,1);                  % number of rats following the configuration
+% cfg.analysis.rat_ids        = rat_ids;                          % 
+% cfg.analysis.nrats          = size(rat_ids,1);                  % number of rats following the configuration
 
-cfg.analysis.sip_sessions   = 1:25;
-cfg.analysis.main_wois      = {'pre_all','pre_event',...
-    'pre_cue','cue','intake','event','no_event'};               % main (time) windows of interest
-cfg.analysis.events         = {'lick','headent'};               % main behavioral events
-cfg.analysis.drink_label    = {'ld','hd'};                      % High and low drinkers label
-cfg.analysis.lick_label     = {'plus','minus'};                      % High and low drinkers label
+cfg.task.sessionEpochs  = {'early','transition','late'};
+cfg.task.cueDuration    = 5;                                % cue duration in sec
+cfg.task.wois = {
+    'pre_all', ...
+    'pre_event', ...
+    'pre_cue', ...
+    'cue', ...
+    'intake', ...
+    'event', ...
+    'no_event'};               
+cfg.task.manipulations  = {'none', 'baseline1', 'baseline2',...
+    'habituation', 'baseline', 'saline', 'dry', 'quinine', 'adlib',...
+    'veh', 'mtep', 'extinction'};  
 
-cfg.analysis.manipulations  = {'none','baseline1','baseline2',...
-    'habituation','baseline','saline','dry','quinine','adlib',...
-    'veh','mtep','extinction'};                                 % manipulation labels
-% 
-% cfg.analysis.late_sessions        = repmat(23:25,16,1);
-% cfg.analysis.late_sessions(3,:)   = 18:20;
-% cfg.analysis.late_sessions(4,:)   = 18:20;
-% cfg.analysis.late_sessions(16,:)  = [21 22 24];
 
-cfg.analysis.cue_dur        = 5;                                % cue duration in sec
-cfg.analysis.int_bout_time  = [1 1.5];                          % maximum time between behavioral events to be considered as part of the same bout
-cfg.analysis.min_bout_dur   = [2 1];                            % minimum duration of one bout
-cfg.analysis.channels       = ch_info;
-cfg.analysis.srate          = 30303; 
+% cfg.analysis.channels       = ch_info;
 
-cfg.analysis.vrate          = 25;         % video sampling rate in frames/s
-cfg.analysis.video_res      = [576 720];  % tracking video resolution
-cfg.analysis.box.width      = 32.5;
-cfg.analysis.box.length     = 31.5;
-cfg.analysis.box.hight      = 40;
-cfg.analysis.box.external   = 40;
-
-video_res                   = cfg.analysis.video_res;
-cfg.analysis.box.video      = [1 1 video_res(2) video_res(2) 1;1 video_res(1) video_res(1) 1 1];
-cfg.analysis.box.grid       = [125 125 465 465 125;85 440 440 85 85];
-cfg.analysis.box.cue        = [480 422];
-cfg.analysis.box.mag        = [465,270];
-cfg.analysis.box.drink      = [75 270];
-cfg.analysis.box.max_dist   = 5;  % [cm] distances less than this denote proximity to, for example, the cue
-
-cfg.ephys.area        = {'ofc','str'};                    % brain areas labels
-cfg.ephys.area_label  = {'ofc','striatum'};               % brain areas labels
-cfg.ephys.cell_types  = {'pyr','int','msn','fsi','cin'};
-cfg.ephys.cell_label  = {'Pyramidal','Interneuron','MSN','FSI','Cholinergic'};
-cfg.ephys.epochs      = {'early','transition','late'};
-cfg.ephys.early       = repmat(1:3,16,1);
-cfg.ephys.transition  = repmat(12:14,16,1);
-cfg.ephys.late        = [[18 19 20];[21 22 23];repmat(18:20,2,1);[23 24 25];...
-                        repmat(23:25,7,1);repmat(23:25,3,1);[21 23 24]];
+cfg.ephys.area       = {'ofc','str'};                    % brain areas labels
+cfg.ephys.areaLabel  = {'OFC','Striatum'};               % brain areas labels
+cfg.spike.cellTypes  = {'pyr','int','msn','fsi','cin'};
+cfg.spike.cellLabel  = {'Pyramidal','Interneuron','MSN','FSI','Cholinergic'};
+% cfg.ephys.early       = repmat(1:3,16,1);
+% cfg.ephys.transition  = repmat(12:14,16,1);
+% cfg.ephys.late        = [[18 19 20];[21 22 23];repmat(18:20,2,1);[23 24 25];...
+%                         repmat(23:25,7,1);repmat(23:25,3,1);[21 23 24]];
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% graphics configuration %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-rat_ref                 = jet(256);
-session_ref             = summer(256);
-cfg.graph.color.rat     = rat_ref(round(linspace(1,256,cfg.analysis.nrats)),:);
-cfg.graph.color.sess    = session_ref(round(linspace(1,256,max(cfg.analysis.sip_sessions))),:);
-cfg.graph.color.woi     = [60 101 176;237 123 46]/255;
-cfg.graph.color.event   = [14 102 62;163 60 15]/255;
-cfg.graph.color.area    = [50 168 82;90 50 168]/255;
-cfg.graph.color.ld      = [128 179 255]/255;
-cfg.graph.color.hd      = [0 51 128]/255;
-
-cfg.graph.color.plus    = [200 52 2]/255;
-cfg.graph.color.minus   = 0.65 * ones(1,3);
-cfg.graph.color.gray    = 0.65 * ones(1,3);
-
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
