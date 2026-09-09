f8e6b2fb9ec93187cb802bf12888292f4735f430
Sergio Conde <124182273+sergio-conde@users.noreply.github.com>
2026-09-09 16:49:00 +0200

    Update psdAnovaStats.m

--- matlab/lfp/psdAnovaStats.m
+++ matlab/lfp/psdAnovaStats.m
@@ -1,31 +1,48 @@
 %%
 clear; clc
-% sip = sip_mainconfig;
-sessions = 'late';
-filename = strcat('psdQuant',sessions,'.mat');
-% load(fullfile(sip.file.proc,filename))
-load('C:\Users\scond\OneDrive\Escritorio\psdQuantlate.mat')
+sip = sip_mainconfig;
+sessions = {'early','late'};
+for isessions = 1:2
+    filename = strcat('psdQuant',sessions{isessions},'.mat');
+    load(fullfile(sip.file.proc,filename));
+    psdContrast.(sessions{isessions}) = psdQuant;
+end
+% load('C:\Users\scond\OneDrive\Escritorio\psdQuantlate.mat')
 
-%% Test different stats configuration
+%% Test different stats configurations for early and late sessions
 
 cfg = [];
 cfg.testArea = 'striatum'; % ofc / striatum
 cfg.based = 'session'; % animal / session
 cfg.testQuant = 'Diff'; % Diff / Ratio
-cfg.testBand = 'Theta';
+cfg.testBand = 'Delta';
 cfg.model = 'mixed';
 if strcmp(cfg.model,'repeated')
     cfg.group = 'hd';
 end
-cfg.testPsd = psdQuant.(cfg.testArea);
+cfg.testPsd = psdContrast.(sessions{isessions}).(cfg.testArea);
 
-clc
-disp(cfg)
 psdTest = sipPsdAnova(cfg);
+
+idLabels = setdiff(fieldnames(psdTest.cfg),{'display'});
+multLabels = {'epochComp','interEpochs','interGroup'};
+summaryAnova = [];
+for iIdVar = 1:numel(idLabels)
+    summaryAnova.(idLabels{iIdVar}) = psdTest.cfg.(idLabels{iIdVar});
+end
+for iModelVar = 1:numel(psdTest.anova.tableLabels)
+    modelLabel = psdTest.anova.modelLabels{iModelVar};
+    tableLabel = strcat('pVal',psdTest.anova.tableLabels{iModelVar});
+    summaryAnova.(tableLabel) = psdTest.anova.mainTest{modelLabel,'pValue'};
+end
+for iCompVar = 1:3
+    summaryAnova.(multLabels{iCompVar}) = psdTest.anova.(multLabels{iCompVar});
+end
 
 
 %%
 plotData = psdTest.anova.data;
+
 % Bar graphs
 meanData = groupsummary(plotData,...
     'Group', ...
@@ -37,17 +54,18 @@ ldMean = getEntry(meanData,'Group','ld');
 
 stdData = groupsummary(plotData,...
     'Group', ...
-    "mean", ...
+    "std", ...
     plotData.Properties.VariableNames(3:end));
+
 errBar = stdData{:,3:end}./sqrt(stdData{:,'GroupCount'});
 hdError = getEntry(stdData,'Group','hd');
 hdError = hdError{1,3:end}./sqrt(hdError.GroupCount);
 ldError = getEntry(stdData,'Group','ld');
 ldError = ldError{1,3:end}./sqrt(ldError.GroupCount);
-yLabel = strcat('Mean ',testBand,'',testQuant);
+yLabel = strcat('Mean ',cfg.testBand,'',cfg.testQuant);
 
 
-hdData = getEntry(testData,'Group','hd');
+hdData = getEntry(plotData,'Group','hd');
 hdData = hdData{:,3:end};
 
 wfig(2); clf
@@ -66,10 +84,10 @@ barCfg.transparency = 0.5;
 bar_disp_dots(hdData,barCfg)
 
 hold off; box off
-set(gca,'xtick',1:5,'XTickLabel',testEpochs)
+set(gca,'xtick',1:5,'XTickLabel',psdTest.cfg.testEpochs)
 ylabel([yLabel ' - HD'])
 
-ldData = getEntry(testData,'Group','ld');
+ldData = getEntry(plotData,'Group','ld');
 ldData = ldData{:,3:end};
 
 hsp(3) = subplot(1,3,3);
@@ -85,27 +103,49 @@ barCfg.transparency = 0.6;
 bar_disp_dots(ldData,barCfg)
 
 hold off; box off
-set(gca,'xtick',1:5,'XTickLabel',testEpochs)
+set(gca,'xtick',1:5,'XTickLabel',psdTest.cfg.testEpochs)
 ylabel([yLabel ' - LD'])
 
 subplot(1,3,1);
 barWidht = 0.3;
-hdBar = bar(1:3:13,mean(hdData), ...
+hdBar = bar(1:3:13,mean(hdData,'omitmissing'), ...
     'FaceColor',sip.graph.color.hd, ...
     'FaceAlpha',0.7,...
     'BarWidth',barWidht);
 hold on
-ldBar = bar(2:3:14,mean(ldData), ...
+ldBar = bar(2:3:14,mean(ldData,'omitmissing'), ...
     'FaceColor',sip.graph.color.ld, ...
     'FaceAlpha',0.8,...
     'BarWidth',barWidht);
 errorbar(1:3:13,hdMean{1,3:end},hdError,'.k')
 errorbar(2:3:14,ldMean{1,3:end},ldError,'.k')
 hold off; box off
-ylabel([yLabel ' @' testArea])
-set(gca,'xtick',1.5:3:13.5,'XTickLabel',testEpochs)
+ylabel([yLabel ' @' psdTest.cfg.testArea])
+set(gca,'xtick',1.5:3:13.5,'XTickLabel',psdTest.cfg.testEpochs)
 legend([hdBar, ldBar],{'HD','LD'}, ...
     'Location','northwest', ...
     'Box','off')
 
 linkaxes(hsp(2:3),'y')
+%% Compare early and late HD
+
+clc
+cfg = [];
+cfg.testArea = 'ofc'; % ofc / striatum
+
+earlyData = getEntry(psdContrast.early.(cfg.testArea),'group','hd');
+earlyData = earlyData(:,1:104);
+earlyData(:,'group') = {'early'};
+
+lateData = getEntry(psdContrast.late.(cfg.testArea),'group','hd');
+lateData(:,'group') = {'late'};
+
+cfg.based = 'session'; % animal / session
+cfg.testQuant = 'Diff'; % Diff / Ratio
+cfg.testBand = 'Theta';
+cfg.model = 'mixed';
+cfg.testPsd = cat(1,earlyData,lateData);
+cfg.display = true;
+
+earlyLateTest = sipPsdAnova(cfg);
+
