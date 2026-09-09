5e70556e44e77cf9e1799e7468c21aaec0e3ae4e
Sergio Conde <124182273+sergio-conde@users.noreply.github.com>
2026-09-09 16:48:50 +0200

    Update sipPsdAnova.m

--- matlab/lfp/functions/sipPsdAnova.m
+++ matlab/lfp/functions/sipPsdAnova.m
@@ -1,5 +1,9 @@
 function psdTest = sipPsdAnova(cfg)
 
+if ~isfield(cfg,'display')
+    cfg.display = false;
+end
+
 % 1. Define the within-subjects factor: Epoch
 testEpochs = {'PreCue','Cue','PreLick','Lick','PostLick'};
 withinDesign = table(categorical(testEpochs'), 'VariableNames', {'Epoch'});
@@ -35,10 +39,12 @@ switch cfg.model
     case 'mixed'
         formulaStr = sprintf('%s-%s ~ Group', testEpochs{1}, testEpochs{end}); 
         modelLabels = {'Group','(Intercept):Epoch','Group:Epoch'};
+        tableLabels = {'Group','Epoch','GroupEpoch'};
     case 'repeated'
         testData = testData(testData.Group == cfg.group,:);
         formulaStr = sprintf('%s-%s ~1', testEpochs{1}, testEpochs{end});
         modelLabels = {'(Intercept):Epoch'};
+        tableLabels = {'Epoch'};
     otherwise
         errorMsg = sprintf('\n--> cfg.model must be "mixed" or "repeated"\n');
         error(errorMsg)
@@ -49,7 +55,9 @@ rm = fitrm(testData, formulaStr, 'WithinDesign', withinDesign);
 
 % Repeated measures ANOVA (within-subject effect + interaction)
 ranovatbl = ranova(rm, 'WithinModel', 'Epoch');
-disp(ranovatbl(modelLabels,'pValue'))
+if cfg.display
+    disp(ranovatbl(modelLabels,'pValue'))
+end
 
 % format default output
 psdTest.cfg = cfg;
@@ -57,33 +65,49 @@ psdTest.cfg.testEpochs = testEpochs;
 psdTest.cfg.withinDesign = withinDesign;
 psdTest.anova.data = testData;
 psdTest.anova.fitModel = rm;
-psdTest.anova.tbl = ranovatbl;
+psdTest.anova.mainTest = ranovatbl;
 psdTest.anova.modelLabels = modelLabels;
+psdTest.anova.tableLabels = tableLabels;
 
-if ranovatbl{"(Intercept):Epoch","pValue"} < 0.05
+if ranovatbl{"(Intercept):Epoch","pValue"} < 0.05 
     psdTest.anova.epochComp = multcompare(rm,'Epoch');
     epochSig = psdTest.anova.epochComp(psdTest.anova.epochComp.pValue < 0.05,:);
-    disp(epochSig)
+    if cfg.display
+        disp(epochSig)
+    end
+else
+    psdTest.anova.epochComp = nan;
 end
 
 % post-hoc tests for mixed ANOVA model
 if strcmp(cfg.model,'mixed')
-    if ranovatbl{"Group","pValue"} < 0.05
+    if ranovatbl{"Group","pValue"} < 0.05 
         psdTest.anova.groupComp = multcompare(rm,'Group');
         sigFlags = psdTest.anova.groupComp.pValue < 0.05;
         groupSig = psdTest.anova.groupComp(sigFlags,:);
-        disp(groupSig)
+        if cfg.display
+            disp(groupSig)
+        end
+    else
+        psdTest.anova.groupComp = nan;
     end
-    if ranovatbl{"Group:Epoch","pValue"} < 0.05
+    if ranovatbl{"Group:Epoch","pValue"} < 0.05 
         psdTest.anova.interEpochs = multcompare(rm,'Epoch','By','Group');
         sigFlags = psdTest.anova.interEpochs.pValue < 0.05;
         epochGroupSig = psdTest.anova.interEpochs(sigFlags,:);
-        disp(epochGroupSig)
+        if cfg.display
+            disp(epochGroupSig)
+        end
 
         psdTest.anova.interGroup = multcompare(rm,'Group','By','Epoch');
         sigFlags = psdTest.anova.interGroup.pValue < 0.05;
         groupEpochSig = psdTest.anova.interGroup(sigFlags,:);
-        disp(groupEpochSig)
+        if cfg.display
+            disp(groupEpochSig)
+        end
+    else
+        psdTest.anova.interEpochs = nan;
+        psdTest.anova.interGroup = nan;
     end
 end
 
