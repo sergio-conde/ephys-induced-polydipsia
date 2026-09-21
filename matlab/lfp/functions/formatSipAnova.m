07265aa26f3e66ca3346ece12107a2901e896b09
Sergio Conde <124182273+sergio-conde@users.noreply.github.com>
2026-09-21 16:54:28 +0200

    Update formatSipAnova.m
    add earlyLate model

--- matlab/lfp/functions/formatSipAnova.m
+++ matlab/lfp/functions/formatSipAnova.m
@@ -4,19 +4,7 @@ testEpochs = {'PreCue','Cue','PreLick','Lick','PostLick'};
 strVarCode = strcat(cfg.testBand,cfg.testQuant);
 switch cfg.model
     case {'mixed','repeated'}
-        idVariables = cfg.testPsd.Properties.VariableNames(1:3);
-        inputVariables = cfg.testPsd.Properties.VariableNames(5:end);
-        switch cfg.based
-            case 'animal'
-                cfg.testPsd = groupsummary(cfg.testPsd,idVariables, ...
-                    @(x)mean(x,'omitmissing'),inputVariables);
-                dataVariables = cfg.testPsd.Properties.VariableNames(5:end);
-            case 'session'
-                dataVariables = inputVariables;
-            otherwise
-                errorMsg = sprintf('\n--> cfg.based must be "animal" or "session"\n');
-                error(errorMsg)
-        end
+        [cfg.testPsd, ~, dataVariables] = basedData(cfg.testPsd,cfg.based,3);
         % ANOVA data selection
         testVarFlags = cellfun(@(x) contains(x,strVarCode),dataVariables);
         testVariables = dataVariables(testVarFlags);
@@ -25,7 +13,6 @@ switch cfg.model
         testData.Properties.VariableNames = ['Rat','Group',testEpochs];
         testData.Group = categorical(testData.Group);
         % ----- PSD data selection and formatting ----- %
-
     case 'earlyLate'
         sessions = {'early','late'};
         for isession = 1:2
@@ -33,20 +20,46 @@ switch cfg.model
             sessLabel = [upper(localSession(1)) localSession(2:end)];
             localLabels = cellfun(@(x)strcat(x,sessLabel),testEpochs, ...
                 'UniformOutput',false);
-
             localData = cfg.testPsd.(localSession).(cfg.testArea)(:,1:104);
-            idVariables = localData.Properties.VariableNames(1:2);
-            inputVariables = localData.Properties.VariableNames(5:end);
             localData = getEntry(localData,'group',cfg.testGroup);
-            localData = groupsummary(localData,idVariables, ...
-                @(x)mean(x,'omitmissing'),inputVariables);
-            dataVariables = localData.Properties.VariableNames(4:end);
+            [localData, idVariables, dataVariables] = basedData(localData,cfg.based,2);
             testVarFlags = cellfun(@(x) contains(x,strVarCode),dataVariables);
             localData = localData(:,[idVariables dataVariables{testVarFlags}]);
             localData.Properties.VariableNames(3:end) = localLabels;
             sessData.(localSession) = localData;
         end
-        testData = innerjoin(sessData.early,sessData.late,'Keys','rat');
-        testData.Properties.VariableNames(1:2) = idVariables;
-        testData(:,8) = [];
+        testData = alignEarlyLate(sessData);
 end
+
+function [data, idVariables, dataVariables] = basedData(data,base,nIds)
+idVariables = data.Properties.VariableNames(1:nIds);
+inputVariables = data.Properties.VariableNames(5:end);
+switch base
+    case 'animal'
+        data = groupsummary(data,idVariables, ...
+            @(x)mean(x,'omitmissing'),inputVariables);
+        dataVariables = data.Properties.VariableNames(5:end);
+    case 'session'
+        dataVariables = inputVariables;
+    otherwise
+        errorMsg = sprintf('\n--> cfg.based must be "animal" or "session"\n');
+        error(errorMsg)
+end
+
+% Take the same number of data points from early and late sessions. This is
+% only relevant if session-based anova is chosen
+function testData = alignEarlyLate(sessData)
+
+tags = unique([sessData.early.tagID;sessData.late.tagID])';
+keepEarly = []; 
+keepLate = [];
+for itag = tags
+    nEarly = find(sessData.early.tagID == itag);
+    nLate = find(sessData.late.tagID == itag);
+    minN = min(numel(nEarly),numel(nLate));
+    keepEarly = cat(1,keepEarly,nEarly(randperm(numel(nEarly),minN)));
+    keepLate = cat(1,keepLate,nLate(randperm(numel(nLate),minN)));
+end
+sessData.early = sessData.early(keepEarly,:);
+sessData.late = sessData.late(keepLate,:);
+testData = cat(2,sessData.early,sessData.late(:,3:7));
