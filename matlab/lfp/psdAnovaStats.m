510ecdf820d4f2c8912b60679b2f3a78ab907416
Sergio Conde <124182273+sergio-conde@users.noreply.github.com>
2026-09-21 16:53:42 +0200

    Update psdAnovaStats.m

--- matlab/lfp/psdAnovaStats.m
+++ matlab/lfp/psdAnovaStats.m
@@ -1,4 +1,5 @@
-%%
+%% Loading early and late quantification
+
 clear; clc
 sip = sip_mainconfig;
 sessions = {'early','late'};
@@ -7,8 +8,7 @@ for isessions = 1:2
     load(fullfile(sip.file.proc,filename));
     psdContrast.(sessions{isessions}) = psdQuant;
 end
-% load('C:\Users\scond\OneDrive\Escritorio\psdQuantlate.mat')
-
+bandLabels = {'Delta','Theta','Beta'};
 
 %% Test different stats configurations for early and late sessions
 
@@ -16,7 +16,6 @@ contrastVari = 'Diff'; % Diff | Ratio | NormDf | Power
 anovaModel = 'mixed'; % mixed | repeated
 anovaBased = 'session'; % animal | session
 
-bandLabels = {'Delta','Theta','Beta'};
 summaryAnova = struct([]);
 nLine = 1;
 for isessions = 1:2
@@ -31,11 +30,11 @@ for isessions = 1:2
             cfg.model = anovaModel;
             cfg.testPsd = psdContrast.(sessions{isessions}).(cfg.testArea);
             psdTest = sipPsdAnova(cfg); % main ANOVA function
-            cfg.testBand = bandLabels;
-            idLabels = setdiff(fieldnames(psdTest.cfg),{'display'});
+            idLabels = setdiff(fieldnames(psdTest.cfg),{'display','testPsd'});
             for iIdVar = 1:numel(idLabels)
                 summaryAnova(nLine).(idLabels{iIdVar}) = psdTest.cfg.(idLabels{iIdVar});
             end
+            summaryAnova(nLine).testPsd = psdTest.anova.data;
             for iModelVar = 1:numel(psdTest.anova.tableLabels)
                 modelLabel = psdTest.anova.modelLabels{iModelVar};
                 tableLabel = strcat('pVal',psdTest.anova.tableLabels{iModelVar});
@@ -54,20 +53,46 @@ psdTest.cfg.testBand = bandLabels;
 disp(psdTest.cfg)
 printSipAnova(summaryAnova)
 
-
-%% Compare early and late HD
+%% Compare early and late sessions within a specific group
 
 clc
-cfg = [];
-cfg.testArea = 'ofc'; % ofc / striatum
-cfg.testGroup = 'hd';
-cfg.based = 'animal'; % animal | session | earlyLate
-cfg.testQuant = 'Diff';
-cfg.testBand = 'Theta';
-cfg.model = 'earlyLate';
-cfg.testPsd = psdContrast;
 
+contrastVari = 'Diff'; % Diff | Ratio | NormDf | Power
+anovaModel = 'earlyLate'; % mixed | repeated | earlyLate
+anovaBased = 'session'; % animal | session
+testGroup = 'hd';
 
-
-
-
+summaryAnova = struct([]);
+nLine = 1;
+for iarea = 1:2
+    for iband = 1:numel(bandLabels)
+        cfg = [];
+        cfg.testPsd = psdContrast;
+        cfg.testGroup = testGroup;
+        cfg.model = anovaModel;
+        cfg.testArea = sip.ephys.area_label{iarea}; % ofc / striatum
+        cfg.based = anovaBased;
+        cfg.testQuant = contrastVari;
+        cfg.testBand = bandLabels{iband};
+        psdTest = sipPsdAnova(cfg); % main ANOVA function
+        idLabels = setdiff(fieldnames(psdTest.cfg),{'display','testPsd'});
+        for iIdVar = 1:numel(idLabels)
+            summaryAnova(nLine).(idLabels{iIdVar}) = psdTest.cfg.(idLabels{iIdVar});
+        end
+        summaryAnova(nLine).testPsd = psdTest.anova.data;
+        for iModelVar = 1:numel(psdTest.anova.tableLabels)
+            modelLabel = psdTest.anova.modelLabels{iModelVar};
+            tableLabel = strcat('pVal',psdTest.anova.tableLabels{iModelVar});
+            summaryAnova(nLine).(tableLabel) = psdTest.anova.mainTest{modelLabel,'pValue'};
+        end
+        for iCompVar = 1:numel(psdTest.anova.multLabels)
+            compLabel = psdTest.anova.multLabels{iCompVar};
+            summaryAnova(nLine).(compLabel) = psdTest.anova.(compLabel);
+        end
+        nLine = nLine + 1;
+    end
+end
+clc
+psdTest.cfg.testBand = bandLabels;
+disp(psdTest.cfg)
+printSipAnova(summaryAnova)
