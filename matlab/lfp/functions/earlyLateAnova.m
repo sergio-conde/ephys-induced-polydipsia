c0eb9d76d274cd51c711c1d2a449468ee7b6dd0a
Sergio Conde <124182273+sergio-conde@users.noreply.github.com>
2026-09-21 16:54:34 +0200

    Update earlyLateAnova.m

--- matlab/lfp/functions/earlyLateAnova.m
+++ matlab/lfp/functions/earlyLateAnova.m
@@ -1,35 +1,48 @@
-
-
+cfg = [];
+cfg.testPsd = psdContrast;
+cfg.testGroup = 'hd';
 cfg.sessions = {'early','late'};
+cfg.model = 'earlyLate';
+cfg.testArea = sip.ephys.area_label{iarea}; % ofc / striatum
+cfg.based = 'session';
+cfg.testQuant = contrastVari;
+cfg.testBand = bandLabels{iband};
 
-earlyLateData = [];
-for isession = 1:2
-    sessData = cfg.testPsd.(cfg.sessions{isession}).(cfg.testArea)(:,1:104);
-    sessData = getEntry(sessData,'group',cfg.testGroup);
-    sessData(:,'group') = sessions(isession);
-    earlyLateData = cat(1,earlyLateData,sessData);
-end
-cfg.testPsd = earlyLateData;
+            
+% psdTest = sipPsdAnova(cfg); % main ANOVA function
+% earlyLateData = [];
+% for isession = 1:2
+%     sessData = cfg.testPsd.(cfg.sessions{isession}).(cfg.testArea)(:,1:104);
+%     sessData = getEntry(sessData,'group',cfg.testGroup);
+%     sessData(:,'group') = sessions(isession);
+%     earlyLateData = cat(1,earlyLateData,sessData);
+% end
+% cfg.testPsd = earlyLateData;
 
-cfg.testPsd = formatSipAnova(cfg);
+% cfg.testPsd = formatSipAnova(cfg);
 
-anovaVars = cfg.testPsd.Properties.VariableNames(3:end);
 
 %% Define the two within-subject factors
+testEpochs = {'PreCue','Cue','PreLick','Lick','PostLick'};
+anovaVars = cfg.testPsd.Properties.VariableNames(3:end);
 
 sessionLevels = repelem({'Early','Late'}, 5)';   % Early x5, Late x5
-epochLevelsRep = repmat(epochLabels', numel(cfg.sessions), 1);    % epochs repeated per session
+epochLevelsRep = repmat(testEpochs', numel(cfg.sessions), 1);    % epochs repeated per session
 
 withinDesign = table(categorical(sessionLevels), categorical(epochLevelsRep), ...
     'VariableNames', {'Session','Epoch'});
 
 % Fit the model — response variables must be in the SAME order as withinDesign2
 formulaStr = sprintf('%s-%s ~ 1', anovaVars{1}, anovaVars{end});
+
+
 rm = fitrm(cfg.testPsd, formulaStr, 'WithinDesign', withinDesign);
 
 % Two-way repeated measures ANOVA: Session, Epoch, and their interaction
 ranovatbl = ranova(rm, 'WithinModel', 'Session*Epoch');
 disp(ranovatbl)
+
+compSession = multcompare(rm, 'Session');
 
 % Compare epochs within Early, and epochs within Late, separately
 compWithinSession = multcompare(rm, 'Epoch', 'By', 'Session',...
@@ -51,3 +64,5 @@ disp(compAcrossSession)
 %     lateRat = lateData(lateData.rat == groupRats(irat),:);
 %     validLate = lateRat{:,5} ~= 0 & ~isnan(lateRat{:,5});
 % end
+
+withinDesign
