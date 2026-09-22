31d3adf2effd61c3aa636459f020770573d7024e
Sergio Conde <124182273+sergio-conde@users.noreply.github.com>
2026-09-22 17:02:00 +0200

    Update trialGen.m

--- matlab/behavior/functions/trialGen.m
+++ matlab/behavior/functions/trialGen.m
@@ -1,4 +1,4 @@
-function [trl,trl_list] = trialGen(cfg)
+function [trl,trlList] = trialGen(cfg)
 
 % This function establishes the trials based on woi, entire sip trials or
 % entire session
@@ -6,110 +6,110 @@ function [trl,trl_list] = trialGen(cfg)
 if ~isfield(cfg,'offset')
     cfg.offset = 0;
 end
-if ~isfield(cfg,'lick_end')
-    cfg.lick_end = false;
+if ~isfield(cfg,'lickEnd')
+    cfg.lickEnd = false;
 end
 
-if isnan(cfg.beh.task_end)
-    cfg.beh.task_end = cfg.beh.end;
+if isnan(cfg.beh.taskEnd)
+    cfg.beh.taskEnd = cfg.beh.end;
 end
 
-if isnan(cfg.beh.task_start)
-    cfg.beh.task_start = cfg.beh.start;
+if isnan(cfg.beh.taskStart)
+    cfg.beh.taskStart = cfg.beh.start;
 end
 
-[~,woi_list] = sip_woi(cfg);
-cue_info   = trial_list(woi_list,cfg);
-lick_info  = woi_list(strcmp(woi_list.woi_label,'lick'),:);
+[~,woiList] = sipWoi(cfg);
+cueInfo   = trialList(woiList,cfg);
+lickInfo  = woiList(strcmp(woiList.woi_label,'lick'),:);
 
 switch cfg.interval
     case 'woi'
-        trl_list = woi_list;
+        trlList = woiList;
     case 'sip_trial'
-        lick_info(lick_info.trial_id == 0,:)    = []; % remove lick before first trial
-        trial_info                              = cue_info;
-        trial_info.t_end(1:end-1)               = trial_info.t_start(2:end);
-        trial_info.t_end(end)                   = cfg.beh.task_end * 1e6;
-        trial_info.t_start                      = trial_info.t_start + cfg.offset;
-        trial_info.sample_start                 = (trial_info.t_start - double(cfg.header.FirstTimeStamp))./cfg.header.TimeStampPerSample + 1;
-        trial_info.sample_end                   = (trial_info.t_end - double(cfg.header.FirstTimeStamp))./cfg.header.TimeStampPerSample + 1;
-        trial_info.duration                     = trial_info.t_end - trial_info.t_start;
-        trial_info.woi_id(:)                    = 0;
-        trial_info.drink(lick_info.trial_id)    = true;
-        trial_info.end_flag(:)                  = false;
-        trl_list                                = trial_info;
+        lickInfo(lickInfo.trial_id == 0,:)   = []; % remove lick before first trial
+        trialInfo                            = cueInfo;
+        trialInfo.t_end(1:end-1)             = trialInfo.t_start(2:end);
+        trialInfo.t_end(end)                 = cfg.beh.taskEnd * 1e6;
+        trialInfo.t_start                    = trialInfo.t_start + cfg.offset;
+        trialInfo.sample_start               = (trialInfo.t_start - double(cfg.header.FirstTimeStamp))./cfg.header.TimeStampPerSample + 1;
+        trialInfo.sample_end                 = (trialInfo.t_end - double(cfg.header.FirstTimeStamp))./cfg.header.TimeStampPerSample + 1;
+        trialInfo.duration                   = trialInfo.t_end - trialInfo.t_start;
+        trialInfo.woi_id(:)                  = 0;
+        trialInfo.drink(lickInfo.trial_id)   = true;
+        trialInfo.end_flag(:)                = false;
+        trlList                              = trialInfo;
 
     case 'lick'
 
-        lick_info(lick_info.trial_id == 0,:)  = []; % remove lick before first trial
-        lick_info(lick_info.nwoi_tr > 1,:)    = []; % take only first lick
+        lickInfo(lickInfo.trial_id == 0,:)  = []; % remove lick before first trial
+        lickInfo(lickInfo.nwoi_tr > 1,:)    = []; % take only first lick
 
-        lick_info.latency     = lick_info.t_start - cue_info.t_start(lick_info.trial_id);
-        lick_info.drink(:)    = true;
-        lick_info.end_flag(:) = false;
+        lickInfo.latency     = lickInfo.t_start - cueInfo.t_start(lickInfo.trial_id);
+        lickInfo.drink(:)    = true;
+        lickInfo.end_flag(:) = false;
 
-       if cfg.lick_end 
-            end_list              = lick_info;
-            end_list.drink(:)     = true;
-            end_list.end_flag(:)  = true;
-            end_list.t_start      = lick_info.t_end;
-            end_list.t_end        = lick_info.t_end + abs(cfg.offset);
-            end_list.sample_start = (end_list.t_start - double(cfg.header.FirstTimeStamp))./cfg.header.TimeStampPerSample + 1;
-            end_list.sample_end   = (end_list.t_end - double(cfg.header.FirstTimeStamp))./cfg.header.TimeStampPerSample + 1;
-            end_list.duration     = end_list.t_end - end_list.t_start;
-            end_list.woi_id(:)    = 3;
-            end_list.woi_label(:) = repmat({"lick"}, height(end_list), 1);
-            end_list.latency(:)   = end_list.t_start - cue_info.t_start(end_list.trial_id);
+       if cfg.lickEnd
+            endList              = lickInfo;
+            endList.drink(:)     = true;
+            endList.end_flag(:)  = true;
+            endList.t_start      = lickInfo.t_end;
+            endList.t_end        = lickInfo.t_end + abs(cfg.offset);
+            endList.sample_start = (endList.t_start - double(cfg.header.FirstTimeStamp))./cfg.header.TimeStampPerSample + 1;
+            endList.sample_end   = (endList.t_end - double(cfg.header.FirstTimeStamp))./cfg.header.TimeStampPerSample + 1;
+            endList.duration     = endList.t_end - endList.t_start;
+            endList.woi_id(:)    = 3;
+            endList.woi_label(:) = repmat({"lick"}, height(endList), 1);
+            endList.latency(:)   = endList.t_start - cueInfo.t_start(endList.trial_id);
 
-            l_minus_end              = cue_info(setdiff(cue_info.trial_id,lick_info.trial_id),:);
-            l_minus_end.drink(:)     = false;
-            l_minus_end.end_flag(:)  = true;
-            l_minus_end.t_start      = l_minus_end.t_start + mean(lick_info.latency) + mean(lick_info.duration);
-            l_minus_end.t_end        = l_minus_end.t_start + max(lick_info.duration);
-            l_minus_end.sample_start = (l_minus_end.t_start - double(cfg.header.FirstTimeStamp))./cfg.header.TimeStampPerSample + 1;
-            l_minus_end.sample_end   = (l_minus_end.t_end - double(cfg.header.FirstTimeStamp))./cfg.header.TimeStampPerSample + 1;
-            l_minus_end.duration     = l_minus_end.t_end - l_minus_end.t_start;
-            l_minus_end.woi_id(:)    = 3;
-            l_minus_end.woi_label(:) = repmat({"lick"}, height(l_minus_end), 1);
-            l_minus_end.latency(:)   = mean(lick_info.latency);
+            lMinusEnd              = cueInfo(setdiff(cueInfo.trial_id,lickInfo.trial_id),:);
+            lMinusEnd.drink(:)     = false;
+            lMinusEnd.end_flag(:)  = true;
+            lMinusEnd.t_start      = lMinusEnd.t_start + mean(lickInfo.latency) + mean(lickInfo.duration);
+            lMinusEnd.t_end        = lMinusEnd.t_start + max(lickInfo.duration);
+            lMinusEnd.sample_start = (lMinusEnd.t_start - double(cfg.header.FirstTimeStamp))./cfg.header.TimeStampPerSample + 1;
+            lMinusEnd.sample_end   = (lMinusEnd.t_end - double(cfg.header.FirstTimeStamp))./cfg.header.TimeStampPerSample + 1;
+            lMinusEnd.duration     = lMinusEnd.t_end - lMinusEnd.t_start;
+            lMinusEnd.woi_id(:)    = 3;
+            lMinusEnd.woi_label(:) = repmat({"lick"}, height(lMinusEnd), 1);
+            lMinusEnd.latency(:)   = mean(lickInfo.latency);
        else
-           end_list     = [];
-           l_minus_end  = [];
+           endList    = [];
+           lMinusEnd  = [];
        end
 
-        l_minus              = cue_info(setdiff(cue_info.trial_id,lick_info.trial_id),:);
-        l_minus.end_flag(:)  = false;
-        l_minus.t_start      = l_minus.t_start + mean(lick_info.latency);
-        l_minus.t_end        = l_minus.t_start + max(lick_info.duration);
-        l_minus.sample_start = (l_minus.t_start - double(cfg.header.FirstTimeStamp))./cfg.header.TimeStampPerSample + 1;
-        l_minus.sample_end   = (l_minus.t_end - double(cfg.header.FirstTimeStamp))./cfg.header.TimeStampPerSample + 1;
-        l_minus.duration     = l_minus.t_end - l_minus.t_start;
-        l_minus.woi_id(:)    = 3;
-        l_minus.woi_label(:) = repmat({"lick"}, height(l_minus), 1);
-        l_minus.latency(:)   = mean(lick_info.latency);
+        lMinus              = cueInfo(setdiff(cueInfo.trial_id,lickInfo.trial_id),:);
+        lMinus.end_flag(:)  = false;
+        lMinus.t_start      = lMinus.t_start + mean(lickInfo.latency);
+        lMinus.t_end        = lMinus.t_start + max(lickInfo.duration);
+        lMinus.sample_start = (lMinus.t_start - double(cfg.header.FirstTimeStamp))./cfg.header.TimeStampPerSample + 1;
+        lMinus.sample_end   = (lMinus.t_end - double(cfg.header.FirstTimeStamp))./cfg.header.TimeStampPerSample + 1;
+        lMinus.duration     = lMinus.t_end - lMinus.t_start;
+        lMinus.woi_id(:)    = 3;
+        lMinus.woi_label(:) = repmat({"lick"}, height(lMinus), 1);
+        lMinus.latency(:)   = mean(lickInfo.latency);
 
-        trl_list = [lick_info;end_list;l_minus;l_minus_end];
-        % trl_list = sortrows(trl_list,"t_start","ascend");
+        trlList = [lickInfo;endList;lMinus;lMinusEnd];
+        % trlList = sortrows(trlList,"t_start","ascend");
 
     otherwise
         error('Invalid requested interval')
 end
 
-trl        = [trl_list.t_start trl_list.t_end];
+trl        = [trlList.t_start trlList.t_end];
 trl(end,3) = 0;
 
 end
 
-function trial_info = trial_list(woi_list,cfg)
+function trialInfo = trialList(woiList,cfg)
 
-trial_info                = woi_list(strcmp(woi_list.woi_label,'cue'),:);
-trial_info.t_end(1:end-1) = trial_info.t_start(2:end);
-trial_info.t_end(end)     = cfg.beh.task_end;
-trial_info.sample_end     = (trial_info.t_end - double(cfg.header.FirstTimeStamp))./cfg.header.TimeStampPerSample + 1;
-trial_info.duration       = trial_info.t_end - trial_info.t_start;
-trial_info.woi_id(:)      = 10;
-trial_info.woi_label(:)   = repmat({"trial"}, height(trial_info), 1);
-trial_info.drink(:)       = false;
+trialInfo                = woiList(strcmp(woiList.woi_label,'cue'),:);
+trialInfo.t_end(1:end-1) = trialInfo.t_start(2:end);
+trialInfo.t_end(end)     = cfg.beh.taskEnd;
+trialInfo.sample_end     = (trialInfo.t_end - double(cfg.header.FirstTimeStamp))./cfg.header.TimeStampPerSample + 1;
+trialInfo.duration       = trialInfo.t_end - trialInfo.t_start;
+trialInfo.woi_id(:)      = 10;
+trialInfo.woi_label(:)   = repmat({"trial"}, height(trialInfo), 1);
+trialInfo.drink(:)       = false;
 
 end
 
