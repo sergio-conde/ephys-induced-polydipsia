0666dd9b765a0921ce7e88362b7bda30ea224e88
Sergio Conde <124182273+sergio-conde@users.noreply.github.com>
2026-09-22 17:01:30 +0200

    Update createBehavior.m

--- matlab/behavior/createBehavior.m
+++ matlab/behavior/createBehavior.m
@@ -7,26 +7,37 @@ ft_defaults
 sip = sipConfig('behavior');
 fileList = sipFileList(sip.folder.support,'behavior');
 
-% Behavior analysis uses .eve or medpc files
-eveIds = getID(fileList.eveFiles);
-medIds = getID(fileList.medFiles);
-
-% Find sessions with only medpc output files available
-[onlyMed, indx] = setdiff(medIds,eveIds,"rows");
-onlyMedFiles = fileList.medFiles(indx);
-
 % TOW DIFFERENT FUNCTIONS (NERULAYNX AND MEDPC) CONVERGING TO THE SAME 
 % OUPUT
 
 % WHITHIN THE BEHAVIORAL FUNCTION: USE MEDPC PROCESSING FROM TOOLBOX
 
+%% Select only files from regular SIP sessions
+cfg = [];
+cfg.sessionID = [1 25];
+cfg.contrast.sessionID = 'range';
+sipMedFiles = getEntry(fileList.behMedFiles,cfg); 
+sipNlynxFiles = getEntry(fileList.behNlynxFiles,cfg);
 %%
+ifile = 1;
+
+cfg = sip.medpc;
+cfg.data = sipMedFiles(ifile).filePath;
+medBehavior = medEvents(cfg);
+
+% extract number of pre-licks from internal counter
+medBehavior.preLicks = medBehavior.cfg.data.C(7);
+
+%%
+
+ifile = 1;
+
 %% all eve files
 
 % ----------configure event info for extraction -----%
-behCfg.events       = sip.behavior.events;
-behCfg.inter_bout   = sip.behavior.intBoutTime;
-behCfg.min_dur      = sip.behavior.minBoutDur;
+behCfg.events      = sip.behavior.events;
+behCfg.interBout   = sip.behavior.intBout;
+behCfg.minBoutDur  = sip.behavior.minBoutDur;
 %----------configure event info for extraction -----%
 
 emptyIds = [];                                                              % initilize variable
@@ -49,12 +60,13 @@ for ifile = 1%:length(file_list.eve)                                        % fo
     behCfg.file = file_list.eve(ifile).file_path;
 
     if ~isempty(behCfg.data)
-        events          = extractEvent(behCfg);                              % extract event information from nlynx
+        events          = nlynxEvents(behCfg);                             % extract event information from nlynx
         behavior(ifile) = format_behavior(events);                          % format and concatenate behavioral info
     else
         emptyIds   = cat(1,emptyIds,ifile);                                 % in case of a problem with nlynx, ev_cfg.data will be empty
     end
     fprintf('done\n')
 end
+
 % behavior(empty_ids)  = [];                                                % remove empty files
 % save([cfg.file.proc '\behavior_general.mat'],'behavior');     
